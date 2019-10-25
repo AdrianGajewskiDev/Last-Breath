095cbd691d6fe33b969f1f60278b1ab345875f3f
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2019-10-25 15:36:58 +0200

    Add a base class for weapons and added Player Controller

---
+++ Weapons/HandGun/HandGun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HandGun : Weapon
+{
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        OnShot += HandleShooting;
+    }
+
+    private void HandleShooting(RaycastHit obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Update()
+    {
+        Shot();        
+    }
+}
---
+++ Weapons/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] int Damage;
+    [SerializeField] int ClipSize;
+    [SerializeField] int CurrentAmmoInClip;
+
+    [SerializeField] float rateOfFire;
+    [SerializeField] float range;
+
+    protected Camera camera;
+
+    bool canFIre;
+
+    public event System.Action<RaycastHit> OnShot;
+
+    public virtual void Shot() 
+    {
+        RaycastHit hit;
+
+        var direction = camera.transform.forward;
+
+
+        if (Physics.Raycast(gameObject.transform.position, direction, out hit, range))
+        {
+            OnShot?.Invoke(hit);
+        }
+    }
+}
