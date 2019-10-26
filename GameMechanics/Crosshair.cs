34ce8212c91ae61ffd1082a498e9b32b74dbcff5
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2019-10-26 09:13:20 +0200

    Added a crosshair script

---
+++ GameMechanics/Crosshair.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField] Texture2D crosshair;
+
+    [SerializeField] float Width;
+    [SerializeField] float Height;
+
+    private void OnGUI()
+    {
+        var x = Screen.width / 2;
+        var y = Screen.height / 2;
+
+        GUI.DrawTexture(new Rect(x,y, Width, Height),crosshair);
+    }
+}
